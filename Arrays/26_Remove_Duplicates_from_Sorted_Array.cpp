70568c0661fb1d57c4d8a3915d45939230103efd
Abhishek Parmar <parmarabhishek121005@gmail.com>
2026-08-06 00:33:48 +0530

    Update and rename 26_Remove_Duplicates.cpp to 26_Remove_Duplicates_from_Sorted_Array.cpp

---
+++ Arrays/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,48 @@
+/*
+Problem : 26. Remove Duplicates from Sorted Array
+
+Platform : LeetCode
+
+Pattern : Two Pointers
+
+Time Complexity : O(n)
+
+Space Complexity : O(1)
+
+Status : Accepted
+
+Key Idea
+---------
+i = Last unique element
+
+j = Explorer
+
+Whenever nums[i] != nums[j],
+copy nums[j] to nums[i+1].
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+   int removeDuplicates(vector<int>& nums){
+    int n = nums.size();
+    int i = 0;
+    
+    for(int j=1; j<n; j++){
+        if(nums[i]!=nums[j]){
+            nums[i+1] = nums[j];
+            i++;
+            
+        }
+       
+    
+    }
+        return i+1;
+        
+        }
+    
+
+};
