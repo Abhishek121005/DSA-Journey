f06c23f73076120fe52018572b6473b6ed0879d8
Abhishek Parmar <parmarabhishek121005@gmail.com>
2026-08-06 12:46:39 +0530

    Update 27_Remove_Element.cpp

--- Arrays/27_Remove_Element.cpp
+++ Arrays/27_Remove_Element.cpp
@@ -1,4 +1,26 @@
 /*
+Problem : 27. Remove element from an array
+
+Platform : LeetCode
+
+Pattern : Two Pointers
+
+Time Complexity : O(n)
+
+Space Complexity : O(1)
+
+Status : Accepted
+
+Key Idea
+---------
+- i = next position where a valid element should be placed. (mtlb this will eventually point to the val and then we will replace its value with the j)
+- j = explorer that traverses the array.
+
+
+- If nums[j] != val:
+      copy nums[j] to nums[i]
+      increment i
+- Return i (count of valid elements).
 
 */
 
