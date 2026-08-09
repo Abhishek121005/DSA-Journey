db17d3fcaafae520659319312b1281d27abb45a2
Abhishek Parmar <parmarabhishek121005@gmail.com>
2026-08-09 22:26:17 +0530

    Update 125_valid_Palindrome.cpp

--- Arrays/125_valid_Palindrome.cpp
+++ Arrays/125_valid_Palindrome.cpp
@@ -1,3 +1,33 @@
+/*
+Problem : 125. Valid palindrome
+
+Platform : LeetCode
+
+Pattern : Two Pointers
+
+Time Complexity : O(n)
+
+Space Complexity : O(1)
+
+Status : Accepted
+
+Key Idea
+---------
+i = moves from forward 
+
+j =  point to the last elem
+
+NEw:::::: isalnum() is used to check all the alphaNumeric values (A-Z, a-z, 0-9).
+Used to convert the string to lowerCase tolower()
+
+
+
+My THOUGHTS:
+one pointer from start another from last we just check by ignoring the special character 
+for every indices the pairs we check if they are equal and last return true  **tolower(s[left]) != tolower(s[right])
+
+*/
+
 
 class Solution {
 public:
