7d36c1ec36377a4c6fdb628f01e3edf69fcc6383
Abhishek Parmar <parmarabhishek121005@gmail.com>
2026-08-15 10:30:03 +0530

    Update 525_Contiguous_array.cpp

--- Arrays/Prefix_sum/525_Contiguous_array.cpp
+++ Arrays/Prefix_sum/525_Contiguous_array.cpp
@@ -1,3 +1,32 @@
 /*
 based on Prefix Sum
 */
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+     unordered_map<int, int> mp;
+     int maxLen = 0;
+     int balance = 0;
+    
+     mp[0] = -1;
+
+     for(int i = 0; i < nums.size(); i++){
+     if(nums[i] == 0){
+        balance--;
+     }
+     if(nums[i] == 1){
+        balance++;
+     }
+     
+     if(mp.find(balance) != mp.end()){
+           int  length = i - mp[balance];
+           maxLen = max(maxLen, length);
+     } 
+     else{
+        mp[balance] = i;
+     }
+     }
+     return maxLen;
+    }
+};
