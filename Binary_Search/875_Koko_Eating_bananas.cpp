a611ff980103471b5ec4c3087320f80cad59a8cd
Abhishek Parmar <parmarabhishek121005@gmail.com>
2026-08-17 23:26:13 +0530

    Update 875_Koko_Eating_bananas.cpp

--- Binary_Search/875_Koko_Eating_bananas.cpp
+++ Binary_Search/875_Koko_Eating_bananas.cpp
@@ -1,5 +1,19 @@
 /* 
+My Thoughts::::
+in this particular question we need to find the minimum bananas koko should eat so that 
+she can eat the whole piles of banana in the given time Hours.
+in this question we will be applying bs on answer ie; for this 
+we have to find the range to range kya hoti h 
+In this question samaj minimum time kese nikalega tu jab koko jo h wo max banana khaegi to ham
+check karenge ki konsa pile max h then wo limit hogi
+and start karegi 1 se kyuki that is the minimum time jo lagega hi
+to range ho jaegi   *** 1 to max(piles)  ******
 
+use bad we will check mid and then ham mid ko check karenge ki piles khane me kitna time lag raha h 
+if jo time h wo given time se kam h mtlb mid jyada h ya fir bol sakte h ki mid ki value ko kam karo then 
+koko kam banana kam khaegi taki time match ho sake and koko ko dhire khana psnd h so high = mid;;
+hoga and else agar jo time h wo given time se jyada aya mtlb we have to increase the mid taki koko jaldi kha sake 
+in the given time to ham low = mid+1; karenge 
 
 */
 
