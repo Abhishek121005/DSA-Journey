0468783a7b9a84d10a83f66a3f0493b52977d7f0
Abhishek Parmar <parmarabhishek121005@gmail.com>
2026-08-18 23:08:10 +0530

    Update 1011_Capacity_To_Ship_Packages_Within_D_Days.cpp

--- Binary_Search/1011_Capacity_To_Ship_Packages_Within_D_Days.cpp
+++ Binary_Search/1011_Capacity_To_Ship_Packages_Within_D_Days.cpp
@@ -1,1 +1,47 @@
 
+/* 
+My thoughts::: :: ::: :
+
+*/
+
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int maxSum = 0;
+        
+            for(int i = 0; i<weights.size(); i++){
+                maxSum += weights[i];
+            }
+        
+        
+        // int low = 1;
+        int low = *max_element(weights.begin(), weights.end());
+        int high = maxSum;
+
+        while(low<high){
+            int mid = (low+high)/2;
+            int wSum = 0;
+            int tDay = 1;
+
+            for(int i = 0; i< weights.size(); i++){
+                wSum += weights[i];
+
+                if(wSum <= mid){
+
+                    continue;
+                }
+                else{
+                    tDay++;
+                    wSum = weights[i];
+                }
+            }
+            if(tDay <= days){
+                high = mid;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
