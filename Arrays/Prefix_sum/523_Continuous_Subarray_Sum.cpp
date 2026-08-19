99eab459ee5dee8637e9be129b5418ebf15e13de
Abhishek Parmar <parmarabhishek121005@gmail.com>
2026-08-19 23:40:00 +0530

    Update 523_Continuous_Subarray_Sum.cpp

--- Arrays/Prefix_sum/523_Continuous_Subarray_Sum.cpp
+++ Arrays/Prefix_sum/523_Continuous_Subarray_Sum.cpp
@@ -2,6 +2,35 @@
 /*
 My Thoughts : ::: :: ::
 
+Just a simple implementation of Prefix Sum with map;
+
+mp[0] = -1;
+why this because we need to calculate the length ***
+
+then just calc the PrefixSum as pSum 
+then calc remainder.
+
+if(mp.find(remainder) != mp.end()){
+            int len = i - mp[remainder];
+
+Then we will just check the remainder value if it already exist in the map 
+then we will just calc the len of the indexes by doing  : int len = i - mp[remainder];
+
+since we are told to check if the len is greater than or equal to  2 : 
+
+****  mp[remainder] = i; This line is just used to add the value of remainder inside the map at the index i;
+
+
+if(len >= 2){ return true;}
+        }
+        else{
+            mp[remainder] = i;
+        }
+        }
+        return false;
+
+
+
 */
 
 class Solution {
